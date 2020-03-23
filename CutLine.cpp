a05aed293bacd29475cd51c4b74caed24c93ffcc
Alice_Mars <yaogongtonghua@foxmail.com>
2020-03-23 11:50:02 +0800

    剪绳子的动态规划和贪心解法

---
+++ CutLine.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int cuttingLine(int length)
+{
+	if(length<2)
+	return 0;
+	if(length==2)
+	return 1;
+	if(length==3)
+	return 2;
+	int timeOf3=length/3;
+	if(length-timeOf3*3==1)
+	timeOf3=1;
+	int timeOf2=(length-timeOf3*3)/2;
+	return (int)(pow(3,timeOf3))*(int)(pow(2,timeOf2));
+}
+int main()
+{
+	int n;
+	cin >> n;
+	cout << cuttingLine(n)<<endl;
+}
