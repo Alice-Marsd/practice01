068af87b82d29dc82f4b6c7015d256b9f4dda201
Alice_Mars <yaogongtonghua@foxmail.com>
2020-03-30 12:33:52 +0800

    倒数第k个节点

---
+++ FindKthToTail.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#define N  3
+#include <E:\fan\createlink.h>
+using namespace std;
+
+elemsn* FindKthToTail(elemsn *head,int k)
+{
+	if(head==NULL || k<=0)
+	return NULL;
+	elemsn *pAhead=head,*pBehind;
+	for(int i=0;i<k-1;++i)
+	{
+		if(pAhead->next)
+		{
+			pAhead=pAhead->next;
+		}else{
+			return NULL;
+		}
+	}
+	pBehind=head;
+	while(pAhead->next)
+	{
+		pAhead=pAhead->next;
+		pBehind=pBehind->next;
+	}
+	return pBehind;
+}
+int main()
+{
+	int a[]={1,2,3,4,5,6};
+	elemsn *head=createlink(a) ;
+	printlink(head);
+	cout<<FindKthToTail(head,3)<<endl;
+}
