d001bb02fe4be9b467da30c6e06ab3819f0bfbbf
Alice_Mars <yaogongtonghua@foxmail.com>
2020-04-04 22:14:49 +0800

    栈的压入弹出

---
+++ isPopOrder.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+bool isPopOrder(int *push,int *pop,int length)
+{
+	bool isPopOrder=false;
+	
+
+	if(push!=NULL && pop!=NULL && length>0)
+	{
+		const int* pNextPush=push;
+		const int* pNextPop=pop;
+		stack<int> stack;
+		
+		while(pNextPop-pop<length)
+		{
+			
+			while(stack.empty() || stack.top()!=*pNextPop)
+			{
+				if(pNextPush-push==length)
+				break;
+				stack.push(*pNextPush);
+				pNextPush++;
+			}
+			
+			if(stack.top()!=*pNextPop)
+			break;
+			stack.pop();
+			pNextPop++;
+			
+		}
+			if(stack.empty() && pNextPop-pop==length)
+			isPopOrder=true;
+			
+	}
+	
+	return isPopOrder;
+	
+}
+int main()
+{
+	int push[]={1,2,3,4,5};
+	int pop[]={5,4,3,2,1};
+	cout <<isPopOrder(push,pop,5)<<endl;
+}
