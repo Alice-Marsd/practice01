3af9de1c7fc446c53ce8ff735d530cfc6c1895a5
Alice_Mars <yaogongtonghua@foxmail.com>
2020-04-08 11:35:47 +0800

    字母排列的所有可能性

---
+++ StringPermutation.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+void Permutation(char* pstr,char* pBegin)
+{
+	if(*pBegin=='\0')
+	printf("%s\t",pstr);
+	else
+	{
+		for(char* pCH=pBegin;*pCH!='\0';++pCH)
+		{
+			char temp=*pCH;
+			*pCH=*pBegin;
+			*pBegin=temp;
+			Permutation(pstr,pBegin+1);
+			temp=*pCH;
+			*pCH=*pBegin;
+			*pBegin=temp;
+		}
+		
+	}
+	
+	
+}
+void Permutation(char* str)
+{
+	if(str==NULL)
+	return ;
+	Permutation(str,str);
+}
+int main()
+{
+	char str[]={'a','b','c'};
+	//cin>> str;
+	Permutation(str);
+}
---
+++ unFinish/SerializeBinaryTrees.cpp
@@ -0,0 +1,12 @@
+#include<iostream> 
+#include<fstream>
+#include<E:\fan\BITree.h>
+using namespace std;
+int main()
+{
+	string str="1(2(4,),3(5,6))";
+	ptree root=Create_BiTree(str.c_str());
+	//Serialize(root,)
+	//流的东西 
+	
+}
