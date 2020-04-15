2184376b0b6df3654655451c76c03699e50a4a20
Alice_Mars <yaogongtonghua@foxmail.com>
2020-04-15 08:42:05 +0800

    排列

--- Combination.cpp
+++ Combination.cpp
@@ -1,28 +1,39 @@
 #include<iostream>
+#include<vector>
+#include<string.h>
 using namespace std;
-void CombinationCore(char* begin,char* end)
+void combinationCore(char* string,int number,vector<char>& result); 
+void Combination(char* string) 
 {
-	if(begin==end)
+	if(string == NULL)
+	return;
+	vector<char> result;
+	int length = strlen(string);
+	for(int i = 1 ; i <= length; i++)
 	{
-		return ;
-	}
-	else{
-		for(char* p=begin;p<=end;++p)
-		{			
-			CombinationCore(p+1,end);
-			printf("%c",*p);	
-		}
-		cout<<endl;
+		combinationCore(string,i,result);
 	}
 }
-void Combination(char* str)
+void combinationCore(char* string,int number,vector<char>& result)
 {
-	if(str==NULL)
-	return ;
-	for(char* p=str;(*p)!='\0';++p)
+	if(string == NULL)
+	return;
+	if(number == 0)
 	{
-		CombinationCore(str,p);
+		for(int i = 0; i < result.size();i++)
+		{
+			cout<<result[i];
+		 } 
+		 cout<<" ";
+		 return;
 	}
+	if(*string == '\0')
+	return;
+	result.push_back(*string);
+	combinationCore(string+1,number-1,result);
+	result.pop_back();
+	combinationCore(string+1,number,result);
+	
 }
 int main()
 {
--- StringCombination.cpp
+++ StringCombination.cpp
@@ -1,25 +1,33 @@
 #include<iostream>
+#include<string.h>
+#include<vector>
 using namespace std;
-void Combination(char* pstr,char* pBegin,int len)
+void Combination(char* pstr,int length,vector<char>& result)
 {
-	if(*pBegin=='\0')
-	printf("%s\n",pstr);
-	else{
-		
-		for(int i=0;i<len;++i)
-		{
-			printf("%s\n",pBegin+i);
-			Combination(pstr,pBegin+1,len+1);
-		}
-	
-		
+	if(pstr==NULL)
+	return ;
+	if(length==0)
+	{
+		for(int i=0;i<result.size();++i)
+		printf("%c",result[i]);
+		cout<<endl;
+		return ;
 	}
+	if(*pstr=='\0')
+	return ;
+	result.push_back(*pstr);
+	Combination(pstr+1,length-1,result);
+	result.pop_back();
+	Combination(pstr+1,length,result);
 }
 void Combination(char* str)
 {
 	if(str==NULL)
 	return ;
-	Combination(str,str,1);
+	vector<char> result;
+	int len=strlen(str);
+	for(int i=1;i<=len;++i)
+	Combination(str,i,result);
 }
 int main()
 {
